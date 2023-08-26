e3913adae01eef62be24eec86e1510d44dc68a68
Abhijit Paul <abhijitpaul0212@gmail.com>
2023-08-27 00:47:39 +0530

    Update routes.py

--- project/routes/main/routes.py
+++ project/routes/main/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from project.routes.main import bp
 
-@bp.route("/")
+@bp.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('/users/login.html')
+    return redirect(url_for('users.login'))
